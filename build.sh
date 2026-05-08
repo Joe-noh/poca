13b8f307399245ce61d4c3082af2061028fa34b8
Joe-noh <goflb.jh@gmail.com>
2026-05-08 21:25:49 +0900

    need vite

--- build.sh
+++ build.sh
@@ -6,7 +6,10 @@ pnpm i --frozen-lockfile --prod --dir assets
 mix deps.get --only prod
 MIX_ENV=prod mix compile
 
-MIX_ENV=prod mix assets.build
+mkdir -p priv/static/.vite
+echo {} > priv/static/.vite/manifest.json
+
+MIX_ENV=prod mix assets.setup
 MIX_ENV=prod mix assets.deploy
 
 MIX_ENV=prod mix phx.gen.release
